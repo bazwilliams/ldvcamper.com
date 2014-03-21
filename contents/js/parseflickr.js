42c466656ed398936634eb68c64dd3cde55a89d0
Barry John Williams <barry@bjw.me.uk>
2014-03-21 23:35:26 +0000

    Added loading gif and text for images

--- contents/js/parseflickr.js
+++ contents/js/parseflickr.js
@@ -3,24 +3,37 @@
 	var flickrelements = $(document).find('.flickrslideshow'),
 		imagethumbTemplate = function (src, description, title) {
 			return '<div class="imagethumb">' +
-				   '<div class="thumb"><img width="64" height="64" class="thumb" src="' + src + '" alt="' + description + '"></div>' +
-				   '<p class="imagetext">' + title + '</p>' +
+				   '<div class="thumb"><img width="32" height="32" class="loading thumb" src="images/712.GIF" alt="Loading"></div>' +
+				   '<p class="imagetext">Loading</p>' +
 				   '</div>';
 				},
-		photoInfoUrl =  "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&format=json&api_key=b1734bd76001217414b2fbdde16cda7c&nojsoncallback=1",
-		photoSizesUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&format=json&api_key=b1734bd76001217414b2fbdde16cda7c&nojsoncallback=1",
-		flickrImage = function (el, id) {
-			$.when(
-				$.getJSON(photoSizesUrl + "&photo_id=" + id),
-				$.getJSON(photoInfoUrl + "&photo_id=" + id)
-			).then(function (sizeData, infoData) {
-				var result = {};
-				result.thumb = _.where(sizeData[0].sizes.size, { "label":"Square" } )[0];
-				result.standard = _.where(sizeData[0].sizes.size, { "label":"Medium 800"} )[0];
-				result.title = infoData[0].photo.title._content;
-				result.description = infoData[0].photo.description._content;
-				result.url = _.where(infoData[0].photo.urls.url, { "type":"photopage" } )[0]._content;
-				el.append(imagethumbTemplate(result.thumb.source, result.description, result.title));
+		flickrJSON = function (method, id) {
+			return $.getJSON("https://api.flickr.com/services/rest/", { 
+				method: 'flickr.photos.'+method,
+				photo_id: id,
+				format: 'json',
+				api_key: 'b1734bd76001217414b2fbdde16cda7c',
+				nojsoncallback: 1
+			});
+		},
+		flickrImage = function (parent, id) {
+			var el = $(imagethumbTemplate());
+			parent.append(el);
+			$.when(flickrJSON('getSizes', id)).then(function (sizeData) {
+				var thumb = _.where(sizeData.sizes.size, { "label":"Square" } )[0],
+					standard = _.where(sizeData.sizes.size, { "label":"Medium 800"} )[0];
+				el.find('img').attr('src', '')
+				el.find('img').removeClass('loading');
+				el.find('img').attr('height', 64)
+				el.find('img').attr('width', 64)
+				el.find('img').attr('src', thumb.source)
+			});
+			$.when(flickrJSON('getInfo', id)).then(function (infoData) {
+				var title = infoData.photo.title._content,
+					description = infoData.photo.description._content,
+					url = _.where(infoData.photo.urls.url, { "type":"photopage" } )[0]._content;
+				el.find('.imagetext').text(title);
+				el.find('img').attr('alt', description);
 			});
 		};
 
